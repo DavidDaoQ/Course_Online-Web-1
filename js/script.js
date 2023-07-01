28c91f1558fa343e960010e6217743f3c385e9de
Dao Dang Quang <dquang.haiphong@gmail.com>
2023-07-01 07:02:27 +0700

    Đồ án Tĩnh

--- js/script.js
+++ js/script.js
@@ -1,117 +1,73 @@
-// Playlist
-let videoList = document.querySelectorAll(".video-list-container .list");
+// let's select all required tags or elements
+const mainVideo = document.querySelector("#main-Video");
+const musicList = document.querySelector(".music-list");
+const playlist = document.getElementById("playlist");
+const AllLessons = document.querySelector(".AllLessons");
+const videoTitle = document.querySelector(".title");
 
-videoList.forEach((vid) => {
-  vid.onclick = () => {
-    console.log(1);
-    videoList.forEach((remove) => {
-      remove.classList.remove("active");
-    });
-    vid.classList.add("active");
-    let src = vid.querySelector(".list-video").src;
-    let title = vid.querySelector(".list-title").innerHTML;
-    document.querySelector(".main-video-container .main-video").src = src;
-    document.querySelector(".main-video-container .main-video").play();
-    document.querySelector(".main-video-container .main-vid-title").innerHTML =
-      title;
-  };
+const ulTag = document.querySelector("ul");
+AllLessons.innerHTML = `${allVideos.length} Lessons`;
+
+let musicIndex = 1;
+window.addEventListener("load", () => {
+  loadMusic(musicIndex);
+  playingNow();
 });
+function playMusic() {
+  mainVideo.play();
+  playlist.classList.add("active");
+}
+function loadMusic(indexNumb) {
+  mainVideo.src = `media/${allVideos[indexNumb - 1].src}.mp4`;
+  videoTitle.innerHTML = `${indexNumb}. ${allVideos[indexNumb - 1].name}`;
+}
 
-// Playlist
+for (let i = 0; i < allVideos.length; i++) {
+  let liTag = `<li li-index="${i + 1}">
+      <div class="row">
+         <span>${i + 1}. ${allVideos[i].name}</span>
+      </div>
+      <video class="${allVideos[i].id}" src="media/${
+    allVideos[i].src
+  }.mp4" style="display: none;" title="${allVideos[i].name}"></video>
+      <span id="${allVideos[i].id}" class="duration"></span>
+   </li>`;
+  playlist.insertAdjacentHTML("beforeend", liTag);
 
-const main_video = document.querySelector(".main-video video");
-const main_video_title = document.querySelector(".main-video .title-1");
-const video_playlist = document.querySelector(".video-playlist .videos");
+  let liVideoDuration = ulTag.querySelector(`#${allVideos[i].id}`);
+  let liVideoTag = ulTag.querySelector(`.${allVideos[i].id}`);
 
-let data = [
-  {
-    id: "a1",
-    title: "manipulate text background",
-    name: "manipulate text background.mp4",
-    duration: "2:47",
-  },
-  {
-    id: "a2",
-    title: "build gauge with css",
-    name: "build gauge with css.mp4",
-    duration: "2:45",
-  },
-  {
-    id: "a3",
-    title: "3D popup card",
-    name: "3D popup card.mp4",
-    duration: "24:49",
-  },
-
-  {
-    id: "a4",
-    title: "customize HTML5 form elements",
-    name: "customize HTML5 form elements.mp4",
-    duration: "3:59",
-  },
-  {
-    id: "a5",
-    title: "custom select box",
-    name: "custom select box.mp4",
-    duration: "4:25",
-  },
-  {
-    id: "a6",
-    title: "embed google map to contact form",
-    name: "embed google map to contact form.mp4",
-    duration: "5:33",
-  },
-  {
-    id: "a7",
-    title: "password strength checker javascript web app",
-    name: "password strength checker javascript web app.mp4",
-    duration: "0:29",
-  },
-
-  {
-    id: "a8",
-    title: "custom range slider",
-    name: "custom range slider.mp4",
-    duration: "1:12",
-  },
-  {
-    id: "a9",
-    title: "animated shopping cart",
-    name: "animated shopping cart.mp4",
-    duration: "3:38",
-  },
-];
-
-data.forEach((video, i) => {
-  let video_element = `
-                <div class="video" data-id="${video.id}">
-                    <img src="img/play.svg" alt="">
-                    <p>${i + 1 > 9 ? i + 1 : "0" + (i + 1)}. </p>
-                    <h3 class="title">${video.title}</h3>
-                    <p class="time">${video.duration}</p>
-                </div>
-    `;
-  video_playlist.innerHTML += video_element;
-});
-
-let videos = document.querySelectorAll(".video");
-videos[0].classList.add("active");
-videos[0].querySelector("img").src = "img/pause.svg";
-
-videos.forEach((selected_video) => {
-  selected_video.onclick = () => {
-    for (all_videos of videos) {
-      all_videos.classList.remove("active");
-      all_videos.querySelector("img").src = "img/play.svg";
+  liVideoTag.addEventListener("loadeddata", () => {
+    let videoDuration = liVideoTag.duration;
+    let totalMin = Math.floor(videoDuration / 60);
+    let totalSec = Math.floor(videoDuration % 60);
+    // if totalSec is less then 10 then add 0 at the beginging
+    totalSec < 10 ? (totalSec = "0" + totalSec) : totalSec;
+    liVideoDuration.innerText = `${totalMin}:${totalSec}`;
+    // adding t duration attribe which we'll use below
+    liVideoDuration.setAttribute("t-duration", `${totalMin}:${totalSec}`);
+  });
+}
+// let's work on play particular song on click
+const allLiTags = playlist.querySelectorAll("li");
+function playingNow() {
+  for (let j = 0; j < allVideos.length; j++) {
+    if (allLiTags[j].classList.contains("playing")) {
+      allLiTags[j].classList.remove("playing");
     }
+    if (allLiTags[j].getAttribute("li-index") == musicIndex) {
+      allLiTags[j].classList.add("playing");
+    }
+    // adding onclick attribute in all li tags
+    allLiTags[j].setAttribute("onclick", "clicked(this)");
+  }
+}
 
-    selected_video.classList.add("active");
-    selected_video.querySelector("img").src = "img/pause.svg";
-
-    let match_video = data.find(
-      (video) => video.id == selected_video.dataset.id
-    );
-    main_video.src = "video/" + match_video.name;
-    main_video_title.innerHTML = match_video.title;
-  };
-});
+function clicked(element) {
+  // getting li index of particular clicked li tag
+  let getIndex = element.getAttribute("li-index");
+  musicIndex = getIndex;
+  loadMusic(musicIndex);
+  playMusic();
+  playingNow();
+}
---
+++ js/video-list.js
@@ -0,0 +1,67 @@
+let allVideos = [
+  {
+    name: "Khai báo biến",
+    src: "Khai báo biến",
+    id: "vid_1",
+  },
+  {
+    name: "Toán tử gán",
+    src: "Toán tử gán",
+    id: "vid_2",
+  },
+  {
+    name: "Toán tử ++ -- với tiền tố _ hậu tố",
+    src: "Toán tử ++ -- với tiền tố _ hậu tố",
+    id: "vid_3",
+  },
+  {
+    name: "Comments",
+    src: "Comments",
+    id: "vid_4",
+  },
+  {
+    name: "Một số hàm built-in",
+    src: "Một số hàm built-in",
+    id: "vid_5",
+  },
+  {
+    name: "Làm quen với toán tử",
+    src: "Làm quen với toán tử",
+    id: "vid_6",
+  },
+  {
+    name: "Toán tử số học (1)",
+    src: "Toán tử số học (1)",
+    id: "vid_7",
+  },
+  {
+    name: "Toán tử chuỗi",
+    src: "Toán tử chuỗi",
+    id: "vid_8",
+  },
+  {
+    name: "Toán tử so sánh trong Javascript phần 1",
+    src: "Toán tử so sánh trong Javascript phần 1",
+    id: "vid_9",
+  },
+  {
+    name: "Kiểu dữ liệu Boolean",
+    src: "Kiểu dữ liệu Boolean",
+    id: "vid_10",
+  },
+  {
+    name: "Câu lệnh điều kiện If",
+    src: "Câu lệnh điều kiện If",
+    id: "vid_11",
+  },
+  {
+    name: "Toán tử logical",
+    src: "Toán tử logical",
+    id: "vid_12",
+  },
+  {
+    name: "Kiểu dữ liệu trong Javascript",
+    src: "Kiểu dữ liệu trong Javascript",
+    id: "vid_13",
+  },
+];
